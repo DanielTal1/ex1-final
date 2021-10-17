83e937ebd94ebb1fb820974b1301779a12720040
moshe35008 <moshe35010@gmail.com>
2021-10-17 10:59:02 -0700

    yes

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,11 +1,35 @@
+#include <cmath>
+float average(float* a,int size){
+    float sum = 0;
+    for(int i=0; i<size; i++){
+        sum+=a[i];
+    }
+    return sum/size;
+}
 // returns the variance of X and Y
-float var(float* x, int size);
+float var(float* x, int size)
+{
+    float sum = 0, var = 0, mean = 0;
+    for(int i = 0 ; i < size ; ++i)
+    {
+        mean += x[i];
+    }
+    mean /= (float)size;
+    for(int i = 0 ; i < size ; ++i)
+    {
+        sum += (x[i] - mean) * (x[i] - mean);
+    }
+    return sum / (float)size;
+}
 
 // returns the covariance of X and Y
 float cov(float* x, float* y, int size);
 
 // returns the Pearson correlation coefficient of X and Y
-float pearson(float* x, float* y, int size);
+float pearson(float* x, float* y, int size)
+{
+    return cov(x, y, size) / (float)cbrt((double)var(x, size) * var(y, size));
+}
 
 class Line{
 public:
@@ -20,11 +44,22 @@ public:
 class Point{
 public:
     float x,y;
-    Point(float x, float y):x(x),y(x){}
+    Point(float x, float y):x(x),y(y){}
 };
 
 // performs a linear regression and return s the line equation
-Line linear_reg(Point** points, int size);
+Line linear_reg(Point** points, int size)
+{
+    float x[size], y[size], a, b;
+    for(int i = 0 ; i < size ; ++i)
+    {
+        x[i] = points[i]->x;
+        y[i] = points[i]->y;
+    }
+    a = cov(x, y, size)/ var(x, size);
+    b = average(y, size) - a * average(x, size);
+    return {a, b};
+}
 
 // returns the deviation between point p and the line equation of the points
 float dev(Point p,Point** points, int size);
